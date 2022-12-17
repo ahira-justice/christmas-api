14703537942c3a74753821821d9fdc636c357d36
Ahira Adefokun <ahira.adefokun@teamapt.com>
2022-12-17 12:29:21 +0100

    Implement file upload to s3

--- app/modules/file/file_service.py
+++ app/modules/file/file_service.py
@@ -1,19 +1,22 @@
 import uuid
 
+import boto3
 from fastapi import UploadFile
 from sqlalchemy.orm import Session
 
 from app.common.data.models import File
+from app.common.domain.config import S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from app.common.exceptions.app_exceptions import NotFoundException
 from app.modules.file.file_dtos import FileResponse
 from app.modules.file.file_mappings import file_to_file_response
 
 
 def upload_file(db: Session, file_upload: UploadFile) -> FileResponse:
-    url = upload_file_to_bucket(file_upload)
+    reference = str(uuid.uuid4())
+    url = upload_file_to_bucket(reference, file_upload)
 
     file = File(
-        reference=str(uuid.uuid4()),
+        reference=reference,
         url=url
     )
 
@@ -24,8 +27,12 @@ def upload_file(db: Session, file_upload: UploadFile) -> FileResponse:
     return file_to_file_response(file)
 
 
-def upload_file_to_bucket(file_upload) -> str:
-    pass
+def upload_file_to_bucket(reference: str, file_upload: UploadFile) -> str:
+    s3 = boto3.resource("s3", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    bucket.upload_fileobj(file_upload.file, reference)
+
+    return f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{reference}"
 
 
 def get_file_by_reference(db: Session, reference: str) -> File:
